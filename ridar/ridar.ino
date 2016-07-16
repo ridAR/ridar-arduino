e30809044b5604c8c3102620e8c5ecc9254f2570
shoaib30 <shoaib30@gmail.com>
2016-07-16 22:57:06 +0530

    added blink

--- ridar/ridar.ino
+++ ridar/ridar.ino
@@ -5,6 +5,12 @@ int RIGHT = 2;
 int LEFT = 3;
 int STR = 4;
 int BACK = 5;
+bool strblk = false;
+bool backblk = false;
+bool rightblk = false;
+bool leftblk = false;
+bool strrtblk = false;
+bool strltblk = false;
 void setup() 
 {  
   ridar.begin(9600);
@@ -28,43 +34,106 @@ void loop()
     input = ridar.read();
     Serial.println(input);
     switch(input) {
-      case '0':
+      case 0:
         digitalWrite(RIGHT, LOW);
         digitalWrite(LEFT, LOW);
         digitalWrite(STR, LOW);
         digitalWrite(BACK, LOW);
+        strblk = false;
+        backblk = false;
+        rightblk = false;
+        leftblk = false;
+        strrtblk = false;
+        strltblk = false;
         ridar.println("Cleared All");
         break;
-      case '1':
+      case 1:
         digitalWrite(STR, HIGH);
+        strblk = false;
         ridar.println("Straight set");
         break;
-      case '2':
+      case 2:
         digitalWrite(BACK, HIGH);
+        backblk = false;
         ridar.println("Back set");
         break;
-      case '3':
+      case 3:
         digitalWrite(RIGHT, HIGH);
+        rightblk = false;
         ridar.println("Right set");
         break;
-      case '4':
+      case 4:
         digitalWrite(LEFT, HIGH);
+        leftblk = false;
         ridar.println("Left set");
         break;
-     case '5':
+     case 5:
         digitalWrite(STR, HIGH);
         digitalWrite(RIGHT, HIGH);
+        strrtblk = false;
         ridar.println("Straight Rigt set");
         break;
-      case '6':
+      case 6:
         digitalWrite(STR, HIGH);
         digitalWrite(LEFT, HIGH);
+        strltblk = false;
         ridar.println("Straight Left set");
         break;
+      case 7:
+        strblk = true;
+        break;
+      case 8:
+        backblk = true;
+        break;
+      case 9:
+        rightblk = true;
+        break;
+      case 10:
+        leftblk = true;
+        break;
+      case 11:
+        strrtblk = true;
+        break;
+      case 12:
+        strltblk = true;
+        break;  
       default:
         ridar.println("Invalid code");
         break;
     }
   }
-  delay(1000);
+  if(strblk || backblk || rightblk || leftblk || strrtblk || strltblk)  {
+    for(int i = 0; i < 5; i++) {
+      if(strblk){
+        digitalWrite(STR, HIGH);
+      }
+      if(backblk){
+        digitalWrite(BACK, HIGH);
+      }
+      if(rightblk){
+        digitalWrite(RIGHT, HIGH);
+      }
+      if(leftblk){
+        digitalWrite(LEFT, HIGH);
+      }
+      if(strrtblk){
+        digitalWrite(STR, HIGH);
+        digitalWrite(RIGHT, HIGH);
+      }
+      if(strltblk){
+        digitalWrite(STR, HIGH);
+        digitalWrite(LEFT, HIGH);
+      }
+      delay(100);
+      digitalWrite(STR, LOW);
+      digitalWrite(BACK, LOW);
+      digitalWrite(RIGHT, LOW);
+      digitalWrite(LEFT, LOW);
+      delay(100);
+    }
+  }
+  else{
+    delay(1000);
+  }
+  
 } 
